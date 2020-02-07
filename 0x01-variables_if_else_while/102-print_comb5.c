868d75f1fa5cc04d6f520efe79a7dfd9a5cd5ab2
FatChicken277 <1637@holbertonschool.com>
2020-02-07 01:05:51 +0000

    0x01-variables_if_else_while exercise

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ *main - print a negative, positive and zero
+ *Return: something
+ */
+int main(void)
+{
+int n, o, m, p;
+for (o = 0; o < 10; o++)
+{
+for (n = 0; n < 10; n++)
+{
+for (p = 0; p < 10; p++)
+{
+for (m = 0; m < 10; m++)
+{
+if (n < m)
+{
+putchar((char)(o + 48));
+putchar((char)(n + 48));
+putchar(32);
+putchar((char)(p + 48));
+putchar((char)(m + 48));
+if (!((m == 9 && p == 9) && (o == 9 && n == 8)))
+{
+putchar(44);
+putchar(32);
+}
+else
+{
+putchar('\n');
+}
+}
+}
+}
+}
+}
+return (0);
+}
