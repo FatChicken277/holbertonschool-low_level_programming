258f308e18ff0bb8738dcf001904f4e38d773c19
FatChicken277 <1637@holbertonschool.com>
2020-02-10 21:58:51 -0500

    8-24_hours.c0x02-functions_nested_loops exersice

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,35 +1,34 @@
 #include "holberton.h"
 /**
- *print_last_digit - Do something
+ *jack_bauer - Do something
  *Return: Return something
- *@n: do something
  */
 void jack_bauer(void)
 {
-  int n, o, m, p;
-  for (o = 0; o < 3; o++)
-    {
-      for (n = 0; n < 10; n++)
-	{
-	  for (p = 0; p < 6; p++)
-	    {
-	      for (m = 0; m < 10; m++)
-		{
-		  if (o == 2 && n == 4 && p == 0 && m == 0)
-		    {
-		      return;
-		    }
-		  else
-		    {
-		      _putchar((char)(o + 48));
-		      _putchar((char)(n + 48));
-		      _putchar(58);
-		      _putchar((char)(p + 48));
-		      _putchar((char)(m + 48));
-		      _putchar('\n');
-		    }
-		}
-	    }
-	}
-    }
+int n, o, m, p;
+for (o = 0; o < 3; o++)
+{
+for (n = 0; n < 10; n++)
+{
+for (p = 0; p < 6; p++)
+{
+for (m = 0; m < 10; m++)
+{
+if (o == 2 && n == 4 && p == 0 && m == 0)
+{
+return;
+}
+else
+{
+_putchar((char)(o + 48));
+_putchar((char)(n + 48));
+_putchar(58);
+_putchar((char)(p + 48));
+_putchar((char)(m + 48));
+_putchar('\n');
+}
+}
+}
+}
+}
 }
