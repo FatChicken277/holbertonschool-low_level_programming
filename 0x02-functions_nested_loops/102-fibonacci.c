e36e2155f6cd93346bbeef58d6a72039c7e4da7b
FatChicken277 <1637@holbertonschool.com>
2020-02-12 00:12:41 +0000

    0x03-debugging exersice

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *main - do something
+ *Return: something
+ */
+int main(void)
+{
+long int t1, t2, tf, c;
+t1 = 1;
+t2 = 2;
+printf("%ld\n", t1);
+printf("%ld\n", t2);
+for (c = 0; c < 50; c++)
+{
+tf = t1 + t2;
+printf("%ld\n", tf);
+t1 = t2;
+t2 = tf;
+}
+return (0);
+}
