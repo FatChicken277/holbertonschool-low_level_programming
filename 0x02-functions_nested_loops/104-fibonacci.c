f8369c231ee6116f8ff6d19779ad9693464862bd
FatChicken277 <1637@holbertonschool.com>
2020-02-11 23:07:01 -0500

    0x02-functions_nested_loops exercise

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-float t1, t2, tf;
+long double t1, t2, tf;
 int c;
 t1 = 1;
 t2 = 2;
-printf("%.f, ", t1);
-printf("%.f, ", t2);
+printf("%.Lf, ", t1);
+printf("%.Lf, ", t2);
 for (c = 0; c < 96; c++)
 {
 tf = t1 + t2;
 t1 = t2;
 t2 = tf;
-printf("%.f", tf);
+printf("%.Lf", tf);
 if (c != 95)
 printf(", ");
 }
