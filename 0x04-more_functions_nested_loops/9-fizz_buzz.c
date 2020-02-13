10006e3a7272381fa3b61dad488038ad0ca245b0
FatChicken277 <1637@holbertonschool.com>
2020-02-13 19:21:35 +0000

    0x04-more_functions_nested_loops exersice

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,29 +5,29 @@
  */
 int main(void)
 {
-  int contador;
-  for (contador = 1; contador < 101; contador++)
-    {
-      if ((contador % 3 == 0) && (contador % 5 == 0))
-	{
-	  printf("FizzBuzz ");
-	}
-      if (contador % 3 == 0)
-	{
-	  printf("Fizz ");
-	}
-      if (contador % 5 == 0)
-	{
-	  if(contador < 100)
-	  printf("Buzz ");
-	  else
-	    printf("Buzz");
-	}
-      if (!((contador % 3 == 0) || (contador % 5 == 0)))
-	{
-	  printf("%d ",contador);
-	}
-    }
-  printf("\n");
-  return (0);
+int contador;
+for (contador = 1; contador < 101; contador++)
+{
+if ((contador % 3 == 0) && (contador % 5 == 0))
+{
+printf("FizzBuzz ");
+}
+if (contador % 3 == 0)
+{
+printf("Fizz ");
+}
+if (contador % 5 == 0)
+{
+if (contador < 100)
+printf("Buzz ");
+else
+printf("Buzz");
+}
+if (!((contador % 3 == 0) || (contador % 5 == 0)))
+{
+printf("%d ", contador);
+}
+}
+printf("\n");
+return (0);
 }
