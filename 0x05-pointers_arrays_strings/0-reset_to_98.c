70875e0b0838d3ec1955a4346a7361741681c2eb
FatChicken277 <1637@holbertonschool.com>
2020-02-18 00:24:04 +0000

    0x05-pointers_arrays_strings exersice

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ *reset_to_98 - do something
+ *@n: do something
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *print_rev - do something
+ *@s: do something
+ *Return: something
+ */
+void print_rev(char *s)
+{
+int c, i;
+i = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+for (c = i; c != -1; c--)
+{
+_putchar(s[c]);
+}
+_putchar('\n');
+}
