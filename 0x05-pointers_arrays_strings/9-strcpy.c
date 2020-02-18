17da73f9a54e7125c204e10d322a20d582a55cff
FatChicken277 <1637@holbertonschool.com>
2020-02-18 21:01:32 +0000

    0x05-pointers_arrays_strings exersice

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ **_strcpy - do something
+ *@src: do something
+ *@dest: do something
+ *Return: something
+ */
+char *_strcpy(char *dest, char *src)
+{
+int c, i, m;
+m = 0;
+i = 0;
+for (i = 0; src[i] != '\0'; i++)
+;
+for (c = 0; c < i; c++)
+{
+dest[m] = src[c];
+m++;
+}
+ return (dest);
+}
+
+
+
