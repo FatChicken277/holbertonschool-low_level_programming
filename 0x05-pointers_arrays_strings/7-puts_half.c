1db20d8874c21e7dec1a166c8841a509d4750fa6
FatChicken277 <1637@holbertonschool.com>
2020-02-18 17:17:49 +0000

    0x05-pointers_arrays_strings exersice

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,12 +6,12 @@
  */
 void puts_half(char *str)
 {
-  int r, i, x;
-  x = 0;
+int r, i, x;
+x = 0;
 for (i = 0; str[i] != '\0'; i++)
 {
 }
-if(i % 2 == 0)
+if (i % 2 == 0)
 x = i / 2;
 else
 x = (i - 1) / 2;
