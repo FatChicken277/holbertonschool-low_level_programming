9b2411a0b9c7850e1e9e4e89f7478b0450d1b6d8
FatChicken277 <1637@holbertonschool.com>
2020-02-18 20:04:29 +0000

    0x05-pointers_arrays_strings exersice

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *puts_half - do something
+ *@str: do something
+ *Return: something
+ */
+void print_array(int *a, int n)
+{
+  int x, r;
+ n = 6; 
+  x = n;
+  for (r = 0; r != x; r++)
+    {
+      if (!(r == x - 1))
+	printf("%d, ",a[r]);
+      else
+	printf("%d",a[r]);
+
+    }
+ printf("\n");
+}
