2b72ccc0a0483d615774583517b521081b8be4dc
FatChicken277 <1637@holbertonschool.com>
2020-02-20 20:08:02 +0000

    0x06-pointers_arrays_strings exersice

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ **_strncmp - do something
+ *@s1: do something
+ *@s2: do something
+ *Return: something
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i, j;
+for (i = 0; s1[i] != '\0'; i++)
+;
+for (j = 0; s2[j] != '\0'; j++)
+;
+if (i > j)
+return (15);
+else if (i < j)
+return (-15);
+else
+return (0);
+}
