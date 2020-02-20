3b288791649f8c4d354b0427117f1b927e78a90b
FatChicken277 <1637@holbertonschool.com>
2020-02-20 20:11:25 +0000

    0x06-pointers_arrays_strings exersice

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,9 @@ for (i = 0; s1[i] != '\0'; i++)
 ;
 for (j = 0; s2[j] != '\0'; j++)
 ;
-if (i > j)
+if (i > j || j < i)
 return (15);
-else if (i < j)
+else if (i < j || j > i)
 return (-15);
 else
 return (0);
