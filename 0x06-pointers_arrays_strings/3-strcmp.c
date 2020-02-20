5d7c6b5278b0c18499908c8fc4e1b71cf7939929
FatChicken277 <1637@holbertonschool.com>
2020-02-20 21:06:37 +0000

    0x06-pointers_arrays_strings exersice

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,15 +7,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i, j;
-for (i = 0; s1[i] != '\0'; i++)
-;
-for (j = 0; s2[j] != '\0'; j++)
-;
-if (i > j || j < i)
-return (15);
-else if (i < j || j > i)
-return (-15);
+int i = 0, n = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] == s2[i])
+{
+n = 0;
+}
 else
-return (0);
+{
+n = (s1[i] - s2[i]);
+break;
+}    
+i++;
+}
+return (n);
 }
