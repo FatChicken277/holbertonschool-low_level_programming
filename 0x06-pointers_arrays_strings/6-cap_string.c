dad17e6f8dab3859521a20173a5e9682183bdd55
FatChicken277 <1637@holbertonschool.com>
2020-02-21 00:50:15 +0000

    0x06-pointers_arrays_strings exersice

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ **cap_string - do something
+ *@s: do somerhing
+ *Return: something
+ */
+char *cap_string(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == '.' || s[i] == 9 || s[i] == '\n' || s[i] == ',' || s[i] == ';' ||
+s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' ||
+s[i] == '{' || s[i] == '}' || s[i] == ' ')
+{
+if (s[i + 1] > 96 && s[i + 1] < 123)
+s[i + 1] = s[i + 1] - 32;
+}
+}
+return (s);
+}
