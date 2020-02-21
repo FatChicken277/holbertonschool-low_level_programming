402e5747db5f116d40da6be4547b0e8a33d487ac
FatChicken277 <1637@holbertonschool.com>
2020-02-20 23:37:57 -0500

    0x06-pointers_arrays_strings exersice

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,20 +7,16 @@
 char *rot13(char *s)
 {
 int i, x = 0;
-char v[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-char v4[] = "nopqrstuvwxyzabcdefghijklm";
-char v2[] = "abcdefghijklmnopqrstuvwxyz";
-char v3[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char v[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char v2[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 for (i = 0; s[i] != '\0'; i++)
 {
 x = 0;
-while (!(v2[x] == s[i] || v3[x] == s[i]) && x < 27)
+while (!(v2[x] == s[i]) && x < 53)
 {
 x++;
 }
 if (v2[x] == s[i])
-s[i] = v4[x];
-else if (v3[x] == s[i])
 s[i] = v[x];
 }
 return (s);
