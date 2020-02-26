b02570124aadf336b19a17d5a1b8994b5067e315
FatChicken277 <1637@holbertonschool.com>
2020-02-26 19:30:43 +0000

    Added 0x08-recursion exersice

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,13 +6,12 @@
  */
 int factorial(int n)
 {
-int i = 0;
-if (n > 0)
-{
-i = n * factorial(n - 1);
-}
-else if (n == 0)
+int i = 1;
+if (n == 0)
 return (1);
-else
+else if (n < 0)
+return (-1);
+if (n > 0)
+i = n * factorial(n - 1);
 return (i);
 }
