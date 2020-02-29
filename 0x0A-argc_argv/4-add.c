67f5535cac32bc0e691bc4ae54b8719064141d06
FatChicken277 <1637@holbertonschool.com>
2020-02-29 17:15:04 -0500

    Update to the 0x0A-argc_argv exersice

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-int a = 0, i;
+  int a = 0, i, o;
 if (argc == 1)
 {
 printf("0\n");
@@ -20,6 +20,14 @@ for (i = 1; i < argc; i++)
 {
 if (isdigit(*argv[i]) != 0)
 {
+for (o = 0; argv[i][o] != '\0'; o++)
+{
+if(isdigit(argv[i][o]) == 0)
+{
+printf("Error\n");
+return (1);
+}
+}
 a = a + atoi(argv[i]);
 }
 else
