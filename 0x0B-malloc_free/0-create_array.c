22421501775304c155dfab08ccb21dee28a13187
FatChicken277 <1637@holbertonschool.com>
2020-03-03 17:47:51 +0000

    Update of the 0x0B-malloc_free exersice

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,10 +15,18 @@ char *create_array(unsigned int size, char c)
 	{
 		return ('\0');
 	}
+
 	v = malloc(size * sizeof(char));
+
+	if (v == '\0')
+	{
+		return ('\0');
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		v[i] = c;
 	}
+
 	return (v);
 }
