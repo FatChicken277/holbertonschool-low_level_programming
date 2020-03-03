285486294f52eb3b02cff5b070c532274c421a24
FatChicken277 <1637@holbertonschool.com>
2020-03-03 18:37:24 +0000

    Added 0x0B-malloc_free exersice

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *_strdup - copy of the string given.
+ *@str: string.
+ *Return: a pointer to the array, or NULL if it fails.
+ */
+char *_strdup(char *str)
+{
+	char *v;
+	unsigned int i, size = 0;
+
+	if (str == '\0')
+	{
+		return ('\0');
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		size++;
+	}
+
+	v = malloc(size * sizeof(char));
+
+	if (v == '\0')
+	{
+		return ('\0');
+	}
+
+	i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		v[i] = str[i];
+	}
+
+	v[i] = '\0';
+
+	return (v);
+}
