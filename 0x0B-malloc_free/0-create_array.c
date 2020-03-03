f5d11e3b52569318c962f4822dfdffd3abbaa8a8
FatChicken277 <1637@holbertonschool.com>
2020-03-03 17:39:33 +0000

    Added 0x0B-malloc_free exersice

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *create_array - creates an array of chars.
+ *@size: size of array.
+ *@c: char.
+ *Return: a pointer to the array, or NULL if it fails.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *v;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return ('\0');
+	}
+	v = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+		v[i] = c;
+	}
+	return (v);
+}
