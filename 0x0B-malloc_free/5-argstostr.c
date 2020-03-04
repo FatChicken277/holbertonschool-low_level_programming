4967f5f4ae9effc79e9e29c5178c90114a46e3a4
FatChicken277 <1637@holbertonschool.com>
2020-03-03 23:40:32 -0500

    Update to the 0x0B-malloc_free exersice

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -8,14 +8,21 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, c = 0;
+	int i, j, c = 0, size = 0;
 	char *v;
 
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
-	v = malloc(ac * sizeof(char *) + 1);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			size++;
+		}
+	}
+	v = malloc((ac + size) * sizeof(char *) + 1);
 	if (v == NULL)
 	{
 		return (NULL);
