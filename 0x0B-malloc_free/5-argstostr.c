0a526320b5d51107b5b7b8354388fa32c62e580e
FatChicken277 <1637@holbertonschool.com>
2020-03-03 22:43:32 -0500

    Added 0x0B-malloc_free exersice

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *argstostr - concatenates all the arguments.
+ *@ac: number of arguments passed.
+ *@av: pointer array which points to each argument passed.
+ *Return: a pointer to the array, or NULL if it fails.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, c = 0;
+	char *v;
+
+	if (ac == 0 || av == 0)
+	{
+		return ('\0');
+	}
+	v = malloc(ac * sizeof(char *));
+	if (v == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			v[c] = av[i][j];
+			c++;
+		}
+		v[c] = '\n';
+		c++;
+	}
+	return (v);
+}
