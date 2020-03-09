cd35ef677b84456b3e8969eaa2ac4b2bde778a1d
FatChicken277 <1637@holbertonschool.com>
2020-03-09 13:56:17 -0500

    Added 0x0E-structures_typedef exersice

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - struct dog.
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
+ *
+ * Description: struct dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
