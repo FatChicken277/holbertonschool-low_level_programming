13746b249539a5c947ed3e70cb00b667992ede8b
FatChicken277 <1637@holbertonschool.com>
2020-03-09 17:40:50 -0500

    Update to the 0x0E-structures_typedef exersice

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
 	for (i = 0; name[i] != '\0'; i++)
 		;
