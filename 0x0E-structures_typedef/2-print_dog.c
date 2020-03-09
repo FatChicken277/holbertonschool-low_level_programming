84a82b1ec61ccdb7feb8eaf07ba115d4b0140251
FatChicken277 <1637@holbertonschool.com>
2020-03-09 14:45:42 -0500

    Added 0x0E-structures_typedef exersice

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *print_dog - prints a struct dog.
+ *@d: struct.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+	{
+		return;
+	}
+	if (d->name == 0)
+		printf("(nil)");
+	else
+	printf("Name: %s\n", d->name);
+	if (d->age == 0)
+		printf("(nil)");
+	else
+	printf("Age: %f\n", d->age);
+	if (d->owner == 0)
+		printf("(nil)");
+	else
+	printf("Name: %s\n", d->owner);
+}
