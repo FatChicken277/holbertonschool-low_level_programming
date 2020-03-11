8e6a350af2b3e5430d153948b13bdc3d40614ef5
FatChicken277 <1637@holbertonschool.com>
2020-03-11 12:35:21 -0500

    Added 1-array_iterator.c exersice

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ *array_iterator - given as a parameter on each element of an array.
+ *@array: int array.
+ *@size: size of array.
+ *@action: function to print.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			(*action) (array[i]);
+		}
+	}
+}
