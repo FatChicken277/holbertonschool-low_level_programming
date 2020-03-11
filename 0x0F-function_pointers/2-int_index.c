41e3eb2275e765041493da819d642407536845d3
FatChicken277 <1637@holbertonschool.com>
2020-03-11 12:44:13 -0500

    Update to the 2-int_index.c exersice

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,24 +10,20 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, o = 0;
 
-	if (array && size && cmp)
+	if (size <= 0)
 	{
-	
-		if (size <= 0)
-		{
-			return (-1);
-		}
-
-		for (i = 0; i < size && o != 1; i++)
-		{
-			o = (*cmp) (array[i]);
-		}
-		i--;
-
-		if (o == 0)
-		{
-			return (-1);
-		}
-		return (i);
+		return (-1);
 	}
+
+	for (i = 0; i < size && o != 1; i++)
+	{
+		o = (*cmp) (array[i]);
+	}
+	i--;
+
+	if (o == 0)
+	{
+		return (-1);
+	}
+	return (i);
 }
