d7327b69f8dc3a57f2f36bf533a9e6aa73e3eab7
FatChicken277 <1637@holbertonschool.com>
2020-03-11 12:39:15 -0500

    Added 2-int_index.c exersice

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+/**
+ *int_index - searches for an integer.
+ *@array: int array.
+ *@size: size of array.
+ *@cmp: verification function.
+ *Return: returns the index of the first element.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, o = 0;
+
+	if (array && size && cmp)
+	{
+	
+		if (size <= 0)
+		{
+			return (-1);
+		}
+
+		for (i = 0; i < size && o != 1; i++)
+		{
+			o = (*cmp) (array[i]);
+		}
+		i--;
+
+		if (o == 0)
+		{
+			return (-1);
+		}
+		return (i);
+	}
+}
