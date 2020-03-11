bf9ae73649d42ca31c161d1f1e56dfc6cb1c268b
FatChicken277 <1637@holbertonschool.com>
2020-03-11 16:33:27 -0500

    Update to the 3-main.c file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,8 +11,8 @@ int main(int argc, char *argv[])
 
 	if (argc == 4)
 	{
-		if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
-		&& *argv[2] != '/' && *argv[2] != '%')
+		if ((*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
+		&& *argv[2] != '/' && *argv[2] != '%') || argv[2][1] != '\0')
 		{
 			printf("Error\n");
 			exit(99);
