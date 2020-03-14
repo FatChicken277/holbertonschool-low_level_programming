656f58bbfef860be969bb6bfe8c81542a71a3243
FatChicken277 <1637@holbertonschool.com>
2020-03-14 12:35:44 -0500

    Fix to 1-print_numbers.c error(conditional)

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,20 +11,21 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int i;
 
-	if (separator != NULL)
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
 	{
-		va_start(list, n);
+		printf("%d", va_arg(list, unsigned int));
 
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(list, unsigned int));
-
-			if (i < n - 1)
+		if (i < n - 1)
+			if (separator != NULL)
+			{
 				printf("%s", separator);
-			else
-				printf("\n");
-		}
+			}
 
-		va_end(list);
+		else
+			printf("\n");
 	}
+
+	va_end(list);
 }
