565d2132b11d9cfb4aafcaa30fb6839d8a3849ad
noroot <noroot@localhost.localdomain>
2020-03-19 19:46:51 -0500

    fix a semicolon

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@
 void print(void) __attribute__((constructor));
 void print (void)
 {
-	printf("You're beat! and yet, you must allow,\n")
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
