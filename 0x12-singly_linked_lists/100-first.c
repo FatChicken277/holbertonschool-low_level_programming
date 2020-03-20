6cdd90dff171941d739e1292865b8f2cbd123a11
noroot <noroot@localhost.localdomain>
2020-03-19 19:44:27 -0500

    100-first.c exersice

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *print - print string before the main function is executed.
+ */
+void print(void) __attribute__((constructor));
+void print (void)
+{
+	printf("You're beat! and yet, you must allow,\n")
+	printf("I bore my house upon my back!\n");
+}
