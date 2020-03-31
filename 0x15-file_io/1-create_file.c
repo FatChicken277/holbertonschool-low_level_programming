5bfe647800662545e9759681df0560a2ac92b676
noroot <noroot@localhost.localdomain>
2020-03-31 13:37:26 -0500

    write null char

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,6 +16,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
+	i++;
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file < 0)
 	{
