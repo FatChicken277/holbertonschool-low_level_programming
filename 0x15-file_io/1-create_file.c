141e7b9171fbfd2009c90bc04dab9d8ecf601631
noroot <noroot@localhost.localdomain>
2020-03-31 13:39:37 -0500

    remove null char and added conditional

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,6 @@ int create_file(const char *filename, char *text_content)
 	}
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
-	i++;
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file < 0)
 	{
@@ -25,6 +24,10 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		write(file, text_content, i);
+	}
+	else
+	{
+		write(file, "\0", 1);
 	}
 	close(file);
 	return (1);
