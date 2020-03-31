601e033ef2483b57af54a26e4ca935a90ffa8bc1
noroot <noroot@localhost.localdomain>
2020-03-31 12:30:05 -0500

    Added holberton.h header file

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/** libs **/
+#include <unistd.h>
+#include <fcntl.h>
+
+/** Prototypes **/
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
