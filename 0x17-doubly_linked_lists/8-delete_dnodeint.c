369dc35cf751ec1e835e0b4a62479536a86deda1
FatChicken277 <1637@holbertonschool.com>
2020-05-03 18:24:42 -0500

    fixed conditional

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -17,6 +17,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (index == 0)
 		{
 			aux = aux->next;
+			aux->prev = NULL;
 			free(*head);
 			(*head) = aux;
 			return (1);
