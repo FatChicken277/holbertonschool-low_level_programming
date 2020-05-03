16fbad59f35fbf8c2a9282560b7452e641e96b1d
FatChicken277 <1637@holbertonschool.com>
2020-05-03 14:34:46 -0500

    added h=NULL conditional

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *aux = *h, *new;
 
+	if (!h)
+		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(&(*h), n));
 	i = dlistint_len(*h) - 1;
