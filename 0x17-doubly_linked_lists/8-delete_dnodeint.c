95fa27c315e2aa6c844701593a734c0d3c72579d
FatChicken277 <1637@holbertonschool.com>
2020-05-03 21:29:28 -0500

    fixed node values

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -34,7 +34,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (aux->next == NULL)
 			new->prev = aux->prev->prev;
 		else
+		{
 			new->next = aux->next;
+			new->next->prev = aux->prev;
+		}
 		free(aux);
 		return (1);
 	}
