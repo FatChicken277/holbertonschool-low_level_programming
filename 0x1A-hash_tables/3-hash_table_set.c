b4196c47d8db0916497e7ec7c23ecae7a92fb202
FatChicken277 <1637@holbertonschool.com>
2020-05-29 17:45:14 +0000

    colision case

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,11 +10,24 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
+	hash_node_t *new;
 
 	if (strlen(key) == 0)
 		return (0);
 	idx = key_index((unsigned char *)key, ht->size);
 
+	if (ht->array[idx])
+	{
+		new = malloc(sizeof(hash_node_t));
+		if (!new)
+			return (0);
+		ht->array[idx]->key = (char *)key;
+		ht->array[idx]->value = strdup(value);
+		ht->array[idx]->next = ht->array[idx];
+		ht->array[idx] = new;
+
+		return (1);
+	}
 	ht->array[idx] = malloc(sizeof(hash_node_t));
 	if (!ht->array[idx])
 		return (0);
