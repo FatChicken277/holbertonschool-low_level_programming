1c7c6cd609eac94bcea1db1a127d3fe7d64466c2
FatChicken277 <1637@holbertonschool.com>
2020-05-29 20:31:52 +0000

    added 4-hash_table_get.c file

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: hash table.
+ * @key: key.
+ * Return: value associated with the element, or NULL if key couldn’t be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i, idx;
+	hash_node_t *aux;
+
+	if (ht || strlen(key) != 0)
+	{
+		idx = key_index((unsigned char *)key, ht->size);
+		if (ht->array[idx])
+		{
+			aux = ht->array[idx];
+			for (i = 0; aux; i++)
+			{
+				if (strcmp(key, aux->key) == 0)
+					return (aux->value);
+				aux = aux->next;
+			}
+			return (NULL);
+		}
+		return (NULL);
+	}
+	return (NULL);
+}
