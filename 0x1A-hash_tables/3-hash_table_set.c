b4d7514c130f86d482fdc6262a8517f6836c5e72
FatChicken277 <1637@holbertonschool.com>
2020-05-29 17:37:22 +0000

    remove the other function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-int add_node(hash_node_t *head, const char *key, const char *value);
 
 /**
  * hash_table_set - adds an element to the hash table.
@@ -15,38 +14,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (strlen(key) == 0)
 		return (0);
 	idx = key_index((unsigned char *)key, ht->size);
-	if (add_node(ht->array[idx], key, value) == 0)
-		return (0);
-	return (1);
-}
 
-/**
- * add_node - adds a new node.
- * @head: head.
- * @key: key.
- * @value: value.
- * Return: 1 if it succeeded, 0 otherwise.
- */
-int add_node(hash_node_t *head, const char *key, const char *value)
-{
-	hash_node_t *new, *aux = head;
+	ht->array[idx] = malloc(sizeof(hash_node_t));
+	if(!ht->array[idx])
+		return(0);
 
-	new = malloc(sizeof(hash_node_t));
-	if (!new)
-		return (0);
-
-	new->key = (char *)key;
-	new->value = (char *)value;
-	new->next = NULL;
-
-	if (!head)
-	{
-		head = new;
-		return (1);
-	}
-	while (aux->next)
-		aux = aux->next;
-	aux->next = new;
+	ht->array[idx]->key = (char *)->key;
+	ht->array[idx]->value = (char *)->value;
+	ht->array[idx]->next = NULL;
 
 	return (1);
 }
