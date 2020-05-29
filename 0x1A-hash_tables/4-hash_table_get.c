86f5b52e9efe2d3f80bb2e61b6258a56f522f2a2
FatChicken277 <1637@holbertonschool.com>
2020-05-29 20:38:42 +0000

    fixed betty style

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,7 +4,7 @@
  * hash_table_get - retrieves a value associated with a key.
  * @ht: hash table.
  * @key: key.
- * Return: value associated with the element, or NULL if key couldn’t be found.
+ * Return: value associated with the element, or NULL.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
