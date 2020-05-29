53c5be4242bd2d596e61bcae9706fdf9f9c87095
FatChicken277 <1637@holbertonschool.com>
2020-05-29 20:47:59 +0000

    change or by and

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,7 +11,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int i, idx;
 	hash_node_t *aux;
 
-	if (ht || strlen(key) != 0 || ht->size)
+	if (ht && strlen(key) != 0 && ht->size)
 	{
 		idx = key_index((unsigned char *)key, ht->size);
 		if (ht->array[idx])
