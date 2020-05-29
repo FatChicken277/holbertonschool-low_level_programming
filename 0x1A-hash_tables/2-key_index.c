cb3656cca5aff38a2475143542a9945f33c1030a
FatChicken277 <1637@holbertonschool.com>
2020-05-29 16:13:21 +0000

    added 2-key_index.c file

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives you the index of a key.
+ * @key: key.
+ * @size: size of the array.
+ * Return: the index at which the key/value.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
